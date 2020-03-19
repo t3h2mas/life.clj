dce567829af774387a875fde1ef600a5cecf5593
Thomas Noe <thomas.noe@madwiremedia.com>
2020-03-18 19:27:25 -0600

    introduce the board

--- src/life/board.clj
+++ src/life/board.clj
@@ -4,3 +4,18 @@
   {:pre [(pos? xlength) (pos? ylength)]}
   (vec (map (fn [_x] (vec (boolean-array xlength false))) (range 0 ylength))))
 
+(defn board-cell-at [board cell]
+  (get-in board [(:x cell) (:y cell)]))
+
+(def board-cell-alive?
+  (complement board-cell-at))
+
+(defn board-toggle-cell [board cell]
+  (let [position [(:x cell) (:y cell)]]
+    (assoc-in
+      board
+      position
+      (not (board-cell-at board cell)))))
+
+;; I LOVE YOU MOM...
+;; How did you get stuff...
--- test/life/board_test.clj
+++ test/life/board_test.clj
@@ -1,9 +1,16 @@
 (ns life.board-test
   (:require [clojure.test :refer :all]
-            [life.board :refer :all]))
+            [life.board :refer :all]
+            [life.cell :refer :all]))
 
 (deftest new-board-test
   (testing "can instantiate a new board with 2x2 dimensions"
     (is (= (new-board 2 2)
            [[false false]
             [false false]]))))
+
+(deftest board-toggle-cell-test
+  (testing "the state of a board cell can be reversed"
+    (is (= (board-toggle-cell (new-board 2 2) (cell 1 1))
+           [[false false]
+            [false true]]))))
